0d9a43a5c506233552269dcb613c0d7448f715b6
EDRipper <euan@hackclub.com>
2025-10-27 09:46:10 -0400

    allow CORS

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(express.static('public'));
 
 // Wildcard route to serve house.tmj
